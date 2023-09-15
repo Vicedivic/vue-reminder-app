8685a4075b03369eff35c5e6c7c6cdbec3f5d221
Victor Fink Danø <vic@revealit.dk>
2023-09-15 15:29:25 +0200

    Accessibility improvements

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,14 +1,24 @@
 <template>
-  <div @dblclick="$emit('toggle-reminder', task.id)" :class="{ reminder: task.reminder, task: true }">
-    <h3>
-      {{ task.text }}
-      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
-    </h3>
-    <p>{{ task.day }}</p>
+  <div :class="{ reminder: task.reminder, task: true }">
+    <div>
+      <h3>{{ task.text }}</h3>
+      <p>{{ task.day }}</p>
+    </div>
+    <div class="action-button-container">
+      <button @click="$emit('toggle-reminder', task.id)" class="reminder-button">
+        <ImportantIcon />
+      </button>
+      <button @click="$emit('delete-task', task.id)" class="delete-button">
+        <XIcon />
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import XIcon from './XIcon.vue';
+import ImportantIcon from './ImportantIcon.vue';
+
 defineProps({
   task: Object,
 });
@@ -22,10 +32,39 @@ defineEmits(['delete-task', 'toggle-reminder']);
   margin: 5px;
   padding: 10px 20px;
   cursor: pointer;
+  display: flex;
+  justify-content: space-between;
 }
 
-.fas {
+.action-button-container {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.action-button-container button {
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid transparent;
+  border-radius: 5px;
+  background: transparent;
+  cursor: pointer;
+  transition: border-color 0.3s;
+}
+
+.action-button-container button:hover {
+  border-color: #333;
+}
+
+.delete-button {
   color: red;
+}
+
+.reminder-button {
+  color: green;
 }
 
 .task.reminder {
